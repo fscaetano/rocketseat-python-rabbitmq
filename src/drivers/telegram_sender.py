6d394991c173975572d1791150886cd2f1c66c84
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-16 15:48:12 -0300

    feat: Added self test (direct call feature) to telegram sender driver

--- src/drivers/telegram_sender.py
+++ src/drivers/telegram_sender.py
@@ -18,8 +18,15 @@ def send_telegram_message(chat_id, message):
     token = os.getenv("TG_BOT_TOKEN")
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
-        "chat_id": chat_id,
+        "chat_id": int(chat_id),
         "text": message
     }
 
     requests.post(url, data=payload)
+
+
+if __name__ == "__main__":
+    chat_id = -4774075310
+    message = "I'm sending the message directly from the command line."
+
+    send_telegram_message(chat_id, message)
